ca11e1b2e495e68d44be596667974c4d14190bb2
pipat1995 <tao.pipat1995@gmail.com>
2021-07-01 17:24:54 +0700

    debug level approve is null

--- app/Http/Controllers/KPI/EvaluateReview/EvaluateReviewController.php
+++ app/Http/Controllers/KPI/EvaluateReview/EvaluateReviewController.php
@@ -171,6 +171,11 @@ class EvaluateReviewController extends Controller
                 
                 # send mail to approved
             } else {
+                if (!$evaluate->nextlevel->exists) {
+                    DB::rollBack();
+                    Log::warning($evaluate->user->name . " ไม่มี Level approve kpi system..");
+                    return $this->errorResponse($evaluate->user->name . " ไม่มี Level approve", 500);
+                }
                 $user_approve = $this->userApproveService->findFirstLevel($evaluate->user_id);
                 $evaluate->status = KPIEnum::draft;
                 $evaluate->comment = $request->comment;

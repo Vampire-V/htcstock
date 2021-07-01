91b66904ccd15bdde9612c7a90fc5c5f74d01c30
pipat1995 <tao.pipat1995@gmail.com>
2021-07-01 09:45:26 +0700

    check findNameUser is not defined

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
     </script> --}}
     <script src="{{asset('assets/js/bootstrap4.5.0/bootstrap.min.js')}}"></script>
-    <script src="{{asset('assets/js/index.js')}}" defer></script>
+    <script src="{{asset('assets/js/index.js')}}" ></script>
     <script defer>
         var locale = {!! json_encode(session('locale')) !!};
     </script>

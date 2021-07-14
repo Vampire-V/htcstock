dfb7ce59bd4c92d7ff8c0c52b9b917a193cd8089
pipat1995 <tao.pipat1995@gmail.com>
2021-07-14 18:45:22 +0700

    weight config check quarter v1.2

--- app/Http/Controllers/KPI/HomeController.php
+++ app/Http/Controllers/KPI/HomeController.php
@@ -178,11 +178,16 @@ class HomeController extends Controller
             $evaluations = $this->evaluateService->scoreFilter($request);
             $is_last = \collect(['03', '06', '09', '12', 'March', 'June', 'September', 'Depcember']);
             $evaluations->each(function ($item) use ($is_last) {
-                if (!$is_last->contains($item->targetperiod->name) && $item->user->degree !== KPIEnum::one) {
-                    $item->weigth = config('kpi.weight')['month'];
-                } else {
+                if ($item->evaluateDetail->groupBy(fn($rule) => $rule->rules->category_id)->count() > 2) {
                     $item->weigth = config('kpi.weight')['quarter'];
+                }else{
+                    $item->weigth = config('kpi.weight')['month'];
                 }
+                // if ($is_last->contains($item->targetperiod->name) || $item->user->degree === KPIEnum::one) {
+                    
+                // } else {
+                    
+                // }
             });
             // dd($evaluations);
             $this->calculation_summary($evaluations);
@@ -197,9 +202,11 @@ class HomeController extends Controller
     {
         $is_last = \collect(['03', '06', '09', '12', 'March', 'June', 'September', 'Depcember']);
         try {
-            if ($request->is_quarter === "true" || $is_last->contains($request->period)) {
+            if ($request->is_quarter === "true" && $request->degree === KPIEnum::one ) {
                 $config = config('kpi.weight')['quarter'];
             }else if ($request->degree !== KPIEnum::one && !$is_last->contains($request->period) && $request->is_quarter !== "true"){
+                $config = config('kpi.weight')['month'];
+            }else if ($request->is_quarter === "true" && $request->degree !== KPIEnum::one) {
                 $config = config('kpi.weight')['month'];
             }
         } catch (\Exception $e) {

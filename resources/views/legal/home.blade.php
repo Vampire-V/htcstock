1eee3676d45f35ea83f18ac1eaf6909493c2064a
pipat1995 <tao.pipat1995@gmail.com>
2021-09-16 15:59:47 +0700

    dashbord active contract label

--- resources/views/legal/home.blade.php
+++ resources/views/legal/home.blade.php
@@ -213,11 +213,11 @@
 <div class="row">
     <div class="col-md-12">
         <div class="main-card mb-3 card">
-            <div class="card-header">Active Users
+            <div class="card-header">Active Contract
                 <div class="btn-actions-pane-right">
                     <div role="group" class="btn-group-sm btn-group">
-                        <button class="active btn btn-focus">Last Week</button>
-                        <button class="btn btn-focus">All Month</button>
+                        {{-- <button class="active btn btn-focus">Last Week</button>
+                        <button class="btn btn-focus">All Month</button> --}}
                     </div>
                 </div>
             </div>
